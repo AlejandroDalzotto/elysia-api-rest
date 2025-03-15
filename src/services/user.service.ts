67dc56b68fab11a3b66cb97e7b057a080c510564
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-03-15 14:47:45 -0300

    refactor UserService: remove unused methods and improve error handling for user retrieval

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,71 +1,28 @@
 import { UserRepository } from '@/repositories/user.repository';
-import type { InsertUser } from '@/db/schema/users.sql';
+import { NotFoundError } from 'elysia';
 
 export abstract class UserService {
+
   static async findAll() {
     const data = UserRepository.getAll()
 
     return data
   }
-  static async findOneById(id: string) { }
-  static async findOneByEmail(email: string) {
-    const data = await UserRepository.getOneByEmail(email)
 
-    return data
-  }
   static async findOneByUsername(username: string) {
-    const data = await UserRepository.getOneByUsername(username)
+    const user = await UserRepository.getOneByUsername(username)
 
-    return data
-  }
-  static async create(newUser: InsertUser) {
-
-    const isEmailAlreadyTaken = await UserRepository.getOneByEmail(newUser.email)
-    if (isEmailAlreadyTaken) {
-      throw new Error(`Email ${newUser.email} is already taken, please provide another.`)
-    }
-
-    const isUsernameAlreadyTaken = await UserRepository.getOneByUsername(newUser.username)
-    if (isUsernameAlreadyTaken) {
-      throw new Error(`Username ${newUser.username} is already taken, please provide another.`)
-    }
-
-    const encryptedPassword = await Bun.password.hash(newUser.password)
-    const data = await UserRepository.save({
-      email: newUser.email,
-      username: newUser.username,
-      password: encryptedPassword
-    })
-
-    return data
-  }
-
-  static async validateCredentials(email: string, password: string) {
-
-    // We should validate if user with a certain email exist.
-    const user = await UserRepository.getOneByEmail(email)
     if (!user) {
-      throw new Error('Invalid or missing email or password')
+      throw new NotFoundError(`User ${username} not found. Please provide a correct username.`)
     }
 
-    const isPasswordValid = await Bun.password.verify(password, user.password)
-
-    if (!isPasswordValid) {
-      throw new Error('Invalid or missing email or password')
-    }
-
-    return {
-      id: user.id,
-      username: user.username
-    }
+    return user
   }
 
-  static async update(id: string, body: InsertUser) { }
-  static async remove(id: string) { }
   static async existByEmail(email: string) {
 
     const data = await UserRepository.getOneByEmail(email)
 
-    return Boolean(data && data?.email)
+    return Boolean(data && data.email)
   }
 }
